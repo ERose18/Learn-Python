4ee290aade0c15b387566adede44dda7aeb35664
NyxxieDandelion <122112833+ERose18@users.noreply.github.com>
2023-05-22 11:48:53 +0100

    Learnt Numbers, Variable Naming, Booleans and Datatypes!

--- beginner-python/python-codecamp-tutorial.py
+++ beginner-python/python-codecamp-tutorial.py
@@ -16,3 +16,56 @@ print("World")
     
 #############
 
+#Naming Conventions!
+
+#For naming in Python you can use both Uppercase and Lowercase naming for your variables such as Result and result.
+#However, names are case sensitive and therefore if you had myname= "Evie", you cannot call it as Myname.. it must be "myname"
+#You can have numbers within the names of your variables but never at the start. for example, result10 is good but 10result is bad.
+#You are able to use an underscore anywhere in the name of the variable such as _result or result_
+#Spaces cannot be in variable names so we must use snake_case. for example you can use "my_result" but not "my result".
+
+#############
+
+#Numbers!
+
+print(10)  # A positive integer
+print(-3000)  # A negative integer
+
+num = 123456789  # Assigning an integer to a variable
+print(num)
+num = -16000  # Assigning a new integer
+print(num)
+
+#in Python, every negative number begins with a -
+
+print(1.00000000005)  # A positive float
+print(-85.6701)  # A negative float
+
+float_num = 1.23456789
+print(float_num)
+
+#In Python, 5 is considered to be an integer while 5.0 is a float.
+
+#template for making a complex number:
+#complex(real, imaginary)
+#Just like the print() statement is used to print values, complex() is used to create complex numbers.
+
+print(complex(10, 20))  # Represents the complex number (10 + 20j)
+print(complex(2.5, -18.2))  # Represents the complex number (2.5 - 18.2j)
+
+complex_1 = complex(0, 2)
+complex_2 = complex(2, 0)
+print(complex_1)
+print(complex_2)
+
+#############
+
+#Boolean!
+
+#In Python we can just use True or False to represent a Boolean value
+
+print(True)
+
+f_bool = False
+print(f_bool)
+
