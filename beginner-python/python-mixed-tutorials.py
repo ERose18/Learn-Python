b4d7ef69a654b8a9442aac5af721641ff3c0f45b
NyxxieDandelion <122112833+ERose18@users.noreply.github.com>
2023-05-22 12:05:00 +0100

    Learnt Strings Basics

--- beginner-python/python-mixed-tutorials.py
+++ beginner-python/python-mixed-tutorials.py
@@ -69,3 +69,60 @@ print(True)
 f_bool = False
 print(f_bool)
 
+#############
+
+#Strings!
+
+print("Harry Potter!")  # Double quotation marks
+
+got = 'Game of Thrones...'  # Single quotation marks
+print(got)
+print("$")  # Single character
+
+empty = ""
+print(empty)  # Just prints an empty line
+
+multiple_lines = '''Triple quotes allows
+multi-line string.'''
+print(multiple_lines)
+
+#The length of a string can be found using the len() function.
+
+random_string = "I am Batman"  # 11 characters
+print(len(random_string))
+
+#every character is given a numerical index based on its position.
+#A string in Python is indexed from 0 to n-1 where n is its length. This means that the index of the first character in a string is 0.
+
+#Each character in a string can be accessed using its index. The index must be closed within square brackets, [], and appended to the string.
+
+batman = "Bruce Wayne"
+
+first = batman[0]  # Accessing the first character
+print(first)
+
+space = batman[5]  # Accessing the empty space in the string
+print(space)
+
+last = batman[len(batman) - 1]
+print(last)
+
+#We can also change our indexing convention by using negative indices.
+
+batman = "Bruce Wayne"
+print(batman[-1])  # Corresponds to batman[10]
+print(batman[-5])  # Corresponds to batman[6]
+
+#Once we assign a value to a string, we can’t update it later.
+
+#assigning a new value to string variable doesn’t mean that you’ve changed the value. Let’s verify it with the id() method below.
+
+str1 = "hello"
+print(id(str1))
+
+str1 = "bye"
+print(id(str1))
+
+#In Python 3.x, all strings are unicode. But, older versions of Python (Python 2.x) support only ASCII characters.
+#To use unicode in Python 2.x, preceding the string with a u is must. For example:
+#string = u"This is unicode"
