1ea5086d38ce5917c72cfeb7e32eb79306f02ede
NyxxieDandelion <122112833+ERose18@users.noreply.github.com>
2023-05-22 11:28:57 +0100

    Currently working through series of free tutorials for beginners in Python

---
+++ beginner-python/python-codecamp-tutorial.py
@@ -0,0 +1,18 @@
+if 5 > 2:
+    print("Five is Larger than two")
+    #if 5 is larger than 2 then print the text within print
+
+#############
+
+my_name = "Evie"
+print("Hello " + my_name + "!")
+    #sets my_name to "Evie" and then prints "Hello Evie!" into the terminal
+
+#############
+
+print("Hello", end=" ")
+print("World")
+    #Each print by defauly will print onto a new line, by using end=" ", the next print will continue on the same line as the previous print
+    
+#############
+
